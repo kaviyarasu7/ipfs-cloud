dfeb8bfc913da500ff9ca41f76ab77629f6b91d5
Alexander Krupenkin <mail@akru.me>
2017-01-13 14:17:52 +0300

    Added dockerfile and entrypoint script

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM debian:jessie 
+MAINTAINER Alexander Krupenkin <mail@akru.me>
+LABEL Description="Cloud IPFS node" Vendor="Airalab" Version="0.1"
+
+ADD http://dist.ipfs.io/go-ipfs/v0.4.4/go-ipfs_v0.4.4_linux-amd64.tar.gz /go-ipfs.tar.gz 
+RUN tar xf go-ipfs.tar.gz && cd go-ipfs && ./install.sh && rm -rf go-ipfs*
+
+ADD docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
+ENTRYPOINT ["docker-entrypoint.sh"]
+
+EXPOSE 4001 5001 80
+VOLUME ["/ipfs"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export IPFS_PATH=/ipfs
+
+# First init
+if [ ! -f /ipfs/config ]; then
+    ipfs init
+    ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
+    ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/80
+    ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+    ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+    ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
+fi
+
+exec ipfs daemon --enable-gc
