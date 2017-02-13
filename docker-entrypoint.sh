a96724b6d828385822f0ca94037f06745cdb846e
Alexander Krupenkin <mail@akru.me>
2017-02-13 17:03:16 +0300

    Added frontend ip variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,16 @@
 
 export IPFS_PATH=/ipfs
 
+if [ -e "$FRONTENDIP" ]; then
+    export FRONTENDIP=0.0.0.0
+fi
+
 # First init
 if [ ! -f /ipfs/config ]; then
     ipfs init
     ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
-    ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/80
+    ipfs config Addresses.Swarm /ip4/${FRONTENDIP}/tcp/4001
+    ipfs config Addresses.Gateway /ip4/${FRONTENDIP}/tcp/80
     ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
     ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
     ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
